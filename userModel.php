092a4160221338d148cdad2a36a2562163417277
Idrissa Sow <100834888+sowidrissa@users.noreply.github.com>
2022-03-10 11:44:53 +0000

    Add files via upload

---
+++ userModel.php
@@ -0,0 +1,35 @@
+<?php
+function find_user_login_password(string $login,string $password):array{
+    $users=json_to_array("users");
+    foreach($users as $user){
+        if ($user['login']==$login && $user['password']==$password){
+            return $user;
+        }
+    }
+    return [];
+}
+
+
+function find_users(string $role):array{
+    $users=json_to_array("users");
+    $result=[];
+    foreach ($users as $user) {
+    if( $user['role']==$role)
+    $result[]= $user;
+    }
+    return $result;
+
+}
+
+function usersArray():array{
+    $tableau=[
+        'prenom'=>$_POST['prenom'],
+        'nom'=>$_POST['nom'],
+        'login'=>$_POST['login'],
+        'password'=>$_POST['password'],
+        'role'=>ROLE_JOUEUR,
+        'score'=>0,
+    ];
+    
+    return $tableau;
+}
