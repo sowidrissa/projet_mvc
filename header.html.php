154ba5928366b9119b50153d3e65fdea4a188579
Idrissa Sow <100834888+sowidrissa@users.noreply.github.com>
2022-03-10 11:45:54 +0000

    Add files via upload

---
+++ header.html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>QUIZZ APP</title>
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT."css/login.css"?>">
+<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap"
+rel="stylesheet">
+<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT."css/style.css"?>">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT."css/inscription.css"?>">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT."css/connexion.style.css"?>">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT."css/style.liste.joueur.css"?>">
+
+<!-- <style>
+    #entete{
+        width: 100vw;
+        height: 18vh;
+    }
+    .lepj{
+        text-align: center;
+    }
+</style> -->
+</head>
+<body style='background-image:url(<?=PATH_PUBLIC.'img'.DIRECTORY_SEPARATOR.'img-bg.jpg'?>);'> 
+<!-- <div id="entete">
+    <img src="<?= PATH_PUBLIC.'img'.DIRECTORY_SEPARATOR.'logo-QuizzSA.png' ?>" alt="logo" id="logo" class="cssLogo">
+    <h1 class="lepj">Le plaisir de jouer</h1>
+</div>  -->
