9a8eea507a8e542bf8b0d1068b9d5915211f956c
Idrissa Sow <100834888+sowidrissa@users.noreply.github.com>
2022-04-26 18:04:10 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-const creernewdiv=document.getElementById("creernewdiv")
-const flex=document.getElementById("flex")
-const buton=document.querySelectorAll("button");
- 
-    for(let i=0;i<buton.length;i++){
-        buton[i].addEventListener('click',function(e) {
-            classM =e.target.classList[0]
-            notification(classM)
-            
-        })
+const container=document.getElementById('container');
+const Precedent=document.querySelector('.Precedent');
+const slide=document.getElementsByName('slide');
+// console.log(Precedent);
+const Suivant=document.querySelector('.Suivant');
+//console.log(Suivant);
+const img=[   'img/img1.jpg',
+                'img/img2.jpg',
+                'img/img3.jpg',
+                'img/img4.jpg',
+                'img/img5.jpg',
+                'img/img6.jpg',
+                'img/img7.jpg',
+                'img/img9.jpg',
+                'img/img10.jpg',
+                'img/img11.jpg',
+                'img/img12.jpg',
 
+
+
+            ];
+            var i=0;
+         
+           
+ var nbr=img.length;      
+function ajouterImage(){
+    document.slide.src=img[i];
+    if (i<nbr-1) {
+        i++;
+        
     }
+    else
+        i=0;
+    setTimeout(ajouterImage,2000);
+}
 
- function notification(classNew){
-     var new1=document.createElement("div");
-     var nomnotification=document.createElement("h1");
-     nomnotification.innerHTML="Mon projet 4";
-     new1.appendChild(nomnotification);
-     new1.className=classNew
-     new1.classList.add("flex")
-     
-     creernewdiv.appendChild(new1); 
-     setTimeout(function(){creernewdiv.removeChild(new1)},2000)
- }
+window.onload=ajouterImage;
+
+Precedent.addEventListener('click',ajouterImage);
+Suivant.addEventListener('click',ajouterImage);
