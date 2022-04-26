5a9b28bdee75d0e233593975f06d3e1655f402ec
Idrissa Sow <100834888+sowidrissa@users.noreply.github.com>
2022-04-26 18:04:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
-const container=document.getElementById('container');
-const Precedent=document.querySelector('.Precedent');
-const slide=document.getElementsByName('slide');
-// console.log(Precedent);
-const Suivant=document.querySelector('.Suivant');
-//console.log(Suivant);
-const img=[   'img/img1.jpg',
-                'img/img2.jpg',
-                'img/img3.jpg',
-                'img/img4.jpg',
-                'img/img5.jpg',
-                'img/img6.jpg',
-                'img/img7.jpg',
-                'img/img9.jpg',
-                'img/img10.jpg',
-                'img/img11.jpg',
-                'img/img12.jpg',
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const count = document.getElementById('count');
+
+    function updateCount(){
+        const currentYear= moment("2023",'YYYY'+1)
+        const now=moment();
+        const difference= currentYear - now ;
+        const nbJours=currentYear.diff(now,"day");
+        const duration=moment.duration(difference,'milliseconds')
+
+        days.textContent = -nbJours;
+        hours.textContent =  -duration._data.hours;
+        minutes.textContent = -duration._data.minutes;
+        seconds.textContent = -duration._data.seconds;
 
 
+ }
 
-            ];
-            var i=0;
-         
-           
- var nbr=img.length;      
-function ajouterImage(){
-    document.slide.src=img[i];
-    if (i<nbr-1) {
-        i++;
-        
-    }
-    else
-        i=0;
-    setTimeout(ajouterImage,2000);
-}
-
-window.onload=ajouterImage;
-
-Precedent.addEventListener('click',ajouterImage);
-Suivant.addEventListener('click',ajouterImage);
+setInterval(()=>{
+    updateCount();
+     count.style.display = 'flex';
+},1000);
